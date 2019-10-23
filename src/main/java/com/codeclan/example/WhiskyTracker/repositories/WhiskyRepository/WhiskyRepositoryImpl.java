03fd9b161d491a5be33bc248aac9063d9e037fe7
Cameron Pellett <bubionbreakfast@gmail.com>
2019-10-23 15:56:46 +0100

    can find whiskies by year

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,5 +1,41 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Whisky> findWhiskiesByYear(int yearToFind) {
+        List<Whisky> result = null;
+
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria criteria = session.createCriteria(Whisky.class);
+
+//            criteria.createAlias("whiskies", "whiskyAlias");
+            criteria.add(Restrictions.eq("year", yearToFind));
+
+            result = criteria.list();
+
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+
+        return result;
+    }
 }
+
